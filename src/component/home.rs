adcaff6e185cd893e27b9a1d725b2c23292f10b7
anu16 <albert.ugwudike16@imperial.ac.uk>
2025-07-14 00:23:43 +0100

    Progress

--- src/component/home.rs
+++ src/component/home.rs
@@ -1,45 +1,60 @@
-use iced::futures::io;
-use iced::widget::image::{viewer, Handle, Viewer};
-use iced::widget::{button, column, container, Image};
+use iced::widget::button::{Appearance, StyleSheet};
+use iced::widget::image::Handle;
+use iced::widget::{button, column, container, row, Button, Image};
 use iced::Alignment::Center;
-use iced::Element;
-use iced::Length;
-use serde::{Serialize, Deserialize};
-use rfd::FileDialog;
-use std::io::Error;
-use std::path::PathBuf;
-use std::{fs};
+use iced::{Background, Border, Color, Element, Renderer, Theme};
 
-use crate::data::Workspace;
-use crate::message::{Message, HomeMessage};
+use crate::message::{HomeMessage, Message};
 
-use Message::Home;
 use HomeMessage::{CreateWorkspace, LoadWorkspace};
+use Message::Home;
 
 #[derive(Debug, Default)]
-pub struct HomePage {
-    value: i64,
-}
+pub struct HomePage;
 
 impl HomePage {
     pub fn new() -> Self {
-        HomePage { value: 0 }
+        HomePage {}
     }
 
     pub fn view<'a>(&self) -> Element<'a, Message> {
-
-        let handle = Handle::from_path("/Users/vaness/projects/microcount-rs/src/assets/microcount_logo.png");
+        let handle = Handle::from_path("./src/assets/microcount_logo.png");
         let image = Image::new(handle).height(200).width(200);
 
         container(
             column![
                 image,
-                button("Load Workspace").on_press(Home(LoadWorkspace)),
-                button("Create Workspace").on_press(Home(CreateWorkspace))
+                row![
+                    ui_button("Load Workspace").on_press(Home(LoadWorkspace)),
+                    button("Create Workspace").on_press(Home(CreateWorkspace))
+                ]
+                .padding(10)
             ]
             .padding(20)
             .align_items(Center),
         )
         .into()
     }
+}
+
+struct CustomButtonStylesheet {}
+
+impl StyleSheet for CustomButtonStylesheet {
+    type Style = iced::Theme;
+
+    fn active(&self, style: &Self::Style) -> Appearance {
+        Appearance {
+            border: Border::with_radius(10),
+            shadow: Default::default(),
+            shadow_offset: Default::default(),
+            background: Some(Background::Color(Color::new(0.0, 1.0, 1.0, 1.0))),
+            text_color: Color::new(1.0, 0.0, 0.0, 1.0),
+        }
+    }
+}
+
+pub fn ui_button<'a, Message>(content: &'static str) -> Button<'a, Message> {
+    button(content).style(iced::theme::Button::Custom(Box::new(
+        CustomButtonStylesheet {},
+    )))
 }
